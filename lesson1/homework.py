ca737dfb6a58e10563097b27d252de99f1dd579a
ottobek <37669297+ottobek@users.noreply.github.com>
2025-05-17 18:30:32 +0500

    Delete lesson1 directory

--- lesson1/homework.py
+++
@@ -1,5 +0,0 @@
-Homework:
-1. Given a side of square. Find its perimeter and area.
-2. Given diameter of circle. Find its length.
-3. Given two numbers a and b. Find their mean.
-4. Given two numbers a and b. Find their sum, product and square of each number.
