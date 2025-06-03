b844a420009941d746d991d460dbd4b3bffba830
ottobek <37669297+ottobek@users.noreply.github.com>
2025-06-03 17:07:24 +0500

    Update homework.py

--- lesson-11/homework/homework.py
+++ lesson-11/homework/homework.py
@@ -5,17 +5,17 @@ Homework:
     - Create string_utils.py module. Define `reverse_string` and `count_vowels` functions in it. (All functions accept one argument in this task)
 3. Create custom packages.
     - Create `geometry` package. 
-    <pre>
+    ```
     geometry\
         __init__.py
         circle.py
-    </pre>
+  ```
     Define `calculate_area` and `calculate_circumference` functions in circle.py. These functions accept one argument(radius).
     - Create `file_operations` package.
-    <pre>
+   ```
     file_operations\
         __init__.py
         file_reader.py
         file_writer.py
-    </pre>
+    ```
     Define `read_file` function in file_reader.py. This function accepts one argument(file_path). Define `write_file` function in file_writer.py. This function accepts two arguments(file_path, content).
