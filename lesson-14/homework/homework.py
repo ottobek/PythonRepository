c6bfa68c633eb8a0acab952da8c956f037b75fa3
ottobek <37669297+ottobek@users.noreply.github.com>
2025-06-03 17:11:25 +0500

    Update homework.py

--- lesson-14/homework/homework.py
+++ lesson-14/homework/homework.py
@@ -1,1 +1,12 @@
+## Homework
 
+1. Task: JSON Parsing
+    - write a Python script that reads the students.jon JSON file and prints details of each student.
+2. Task: Weather API
+   1. Use this url : https://openweathermap.org/
+   2. Use the requests library to fetch weather data for a specific city(ex. your hometown: Tashkent) and print relevant information (temperature, humidity, etc.).
+3. Task: JSON Modification
+   1. Write a program that allows users to add new books, update existing book information, and delete books from the books.json JSON file.
+4. Task: Movie Recommendation System
+   1. Use this url http://www.omdbapi.com/ to fetch information about movies.
+   2. Create a program that asks users for a movie genre and recommends a random movie from that genre.
